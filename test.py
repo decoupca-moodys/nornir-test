74b6e6d845034309fa37a07537ae43ccbf109321
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2022-01-03 14:31:23 -0600

    use netmiko instead

--- test.py
+++ test.py
@@ -1,30 +1,23 @@
 from pprint import pprint
 
+
+import os
 import ipdb
 from nornir import InitNornir
 from nornir.core.inventory import Host
-from netbane.plugins.tasks.netbane_get import netbane_get
-from netbane.plugins.connections import Netbane
-#from nornir_napalm.plugins.tasks.napalm_get import napalm_get
+from netbane.plugins.inventory.netbox import netbox_transform
+from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
 from nornir.core.plugins.inventory import TransformFunctionRegister
-from nornir.core.plugins.connections import ConnectionPluginRegister
-
-
-def netbox_transform(host: Host) -> None:
-    # store raw netbox data in separate key in case we want it later
-    host.data["netbox"] = host.data
-    netbox = host.data["netbox"]
-    host.data["site"] = netbox["site"]["slug"].lower()
-
 
 TransformFunctionRegister.register("netbox_transform", netbox_transform)
-ConnectionPluginRegister.register("netbane", Netbane)
+
+os.environ["NET_TEXTFSM"] = '/usr/local/lib/python3.9/site-packages/ntc_templates/templates'
 
 nr = InitNornir(config_file="config.yaml")
 host = nr.filter(name='SWTC21AC02')
 
-result = host.run(task=netbane_get, getters=['cdp_neighbors'])
+result = host.run(task=netmiko_send_command, command_string="show cdp neighbors detail", use_textfsm=True)
 print_result(result)
 
 ipdb.set_trace()
