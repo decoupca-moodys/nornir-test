7e68edc4baf625064a5b6ed2a14d93acd5c2832b
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2022-01-03 14:32:01 -0600

    black

--- test.py
+++ test.py
@@ -1,23 +1,28 @@
+import os
 from pprint import pprint
 
-
-import os
 import ipdb
+from netbane.plugins.inventory.netbox import netbox_transform
 from nornir import InitNornir
 from nornir.core.inventory import Host
-from netbane.plugins.inventory.netbox import netbox_transform
+from nornir.core.plugins.inventory import TransformFunctionRegister
 from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
-from nornir.core.plugins.inventory import TransformFunctionRegister
 
 TransformFunctionRegister.register("netbox_transform", netbox_transform)
 
-os.environ["NET_TEXTFSM"] = '/usr/local/lib/python3.9/site-packages/ntc_templates/templates'
+os.environ[
+    "NET_TEXTFSM"
+] = "/usr/local/lib/python3.9/site-packages/ntc_templates/templates"
 
 nr = InitNornir(config_file="config.yaml")
-host = nr.filter(name='SWTC21AC02')
+host = nr.filter(name="SWTC21AC02")
 
-result = host.run(task=netmiko_send_command, command_string="show cdp neighbors detail", use_textfsm=True)
+result = host.run(
+    task=netmiko_send_command,
+    command_string="show cdp neighbors detail",
+    use_textfsm=True,
+)
 print_result(result)
 
 ipdb.set_trace()
