e483bfab86c90d3fc9b4a312e7cb6b05147e425d
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2022-01-04 15:26:28 -0600

    playing with tasks

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from netbane.plugins.inventory.netbox import netbox_transform
 from nornir import InitNornir
 from nornir.core.inventory import Host
 from nornir.core.plugins.inventory import TransformFunctionRegister
+from nornir.core.task import Result, Task
 from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
 
@@ -18,11 +19,39 @@ os.environ[
 nr = InitNornir(config_file="config.yaml")
 host = nr.filter(name="SWTC21AC02")
 
-result = host.run(
-    task=netmiko_send_command,
-    command_string="show cdp neighbors detail",
-    use_textfsm=True,
-)
+
+def get_config_and_facts(task: Task):
+    task.run(
+        name="get config",
+        task=netmiko_send_command,
+        command_string="show running-config",
+    )
+    task.run(
+        name="get CDP neighbors",
+        task=netmiko_send_command,
+        command_string="show cdp neighbors detail",
+        use_textfsm=True,
+    )
+    task.run(
+        name="get facts",
+        task=netmiko_send_command,
+        command_string="show version",
+        use_textfsm=True,
+    )
+
+
+def get_wlc_ap_list(task: Task):
+    task.host.open_connection(
+        "netmiko", platform="cisco_wlc_ssh", configuration=task.nornir.config
+    )
+    task.run(
+        task=netmiko_send_command, command_string="show ap summary", use_textfsm=True
+    )
+    task.host.close_connection("netmiko")
+
+
+wlc = nr.filter(name="WWTC19WC11")
+result = wlc.run(task=get_wlc_ap_list)
 print_result(result)
 
 ipdb.set_trace()
